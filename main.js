37273622ae79c815551ea3e16826fd6ea8c77f49
Amir Yankolovich <amiryankolovich2000@gmail.com>
2023-11-15 16:59:04 +0200

    Limit ball size

--- main.js
+++ main.js
@@ -3,6 +3,9 @@
 var ballSize = 100
 function onBallClick(el) {
   ballSize = ballSize + 50
+  if (ballSize > 400) {
+    ballSize = 100
+  }
   el.style.width = ballSize + 50 + "px"
   el.style.height = ballSize + 50 + "px"
   el.innerText = ballSize
