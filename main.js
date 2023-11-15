08be8424efabf14da8d054c335723570bf7831bc
Amir Yankolovich <amiryankolovich2000@gmail.com>
2023-11-15 17:44:16 +0200

    Increase ball size randomly

--- main.js
+++ main.js
@@ -2,11 +2,11 @@
 
 var ballSize = 100
 function onBallClick(el) {
-  ballSize = ballSize + 50
+  ballSize = ballSize + getRandomInt(20, 60)
   if (ballSize > 400) {
     ballSize = 100
   }
-  el.style.width = ballSize + 50 + "px"
-  el.style.height = ballSize + 50 + "px"
+  el.style.width = ballSize + "px"
+  el.style.height = ballSize + "px"
   el.innerText = ballSize
 }
